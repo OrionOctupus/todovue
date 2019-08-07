e0f8e2fdc07300c970856ae14cf03088dbbd6e0a
OrionOctupus <orion.octupus@gmail.com>
2019-08-07 21:36:52 +0300

    first comm

---
+++ src/components/TodoItems.vue
@@ -0,0 +1,5 @@
+<template>
+    <li>
+        todo second
+    </li>
+</template>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <ul>
+            <TodoItems/>
+        </ul>
+    </div>
+</template>
+
+<script>
+import TodoItems from '@/components/TodoItems';
+export default {
+    props: ['namevar'],
+    components : {
+        TodoItems
+    }
+}
+</script>
